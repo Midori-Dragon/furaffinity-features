a36b40f562ef3d412d56f10713ce2571c57205d2
Midori-Dragon <midorinotsumedesu@gmail.com>
2025-03-05 09:45:53 +0100

    updated according to fa update

--- src/feature-modules/FA-Instant-Nuker/src/modules/SubmissionNuker.ts
+++ src/feature-modules/FA-Instant-Nuker/src/modules/SubmissionNuker.ts
@@ -4,13 +4,19 @@ import { NukeButton } from '../components/NukeButton';
 export class SubmissionNuker {
     constructor() {
         const standardPage = document.getElementById('standardpage');
-        const actionsSection = standardPage?.querySelector('section[class*="actions-section"]');
-        const sectionOptions = actionsSection?.querySelector('div[class*="section-options"]');
-        if (sectionOptions == null) {
+        const actionsSection = standardPage?.querySelectorAll('section[class*="actions-section"]');
+        if (actionsSection == null) {
             return;
         }
 
-        const nukeButton = new NukeButton(MessageType.Submission);
-        sectionOptions.appendChild(nukeButton.nukeButton);
+        for (const section of Array.from(actionsSection)) {
+            const sectionOptions = section.querySelector('div[class*="section-options"]');
+            if (sectionOptions == null) {
+                continue;
+            }
+
+            const nukeButton = new NukeButton(MessageType.Submission);
+            sectionOptions.appendChild(nukeButton.nukeButton);
+        }
     }
 }
