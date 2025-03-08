bd9684bc1452b436fdcca1ffa917d8a48dc5679e
Midori-Dragon <midorinotsumedesu@gmail.com>
2025-03-08 10:41:56 +0100

    updated version

--- src/library-modules/Furaffinity-Features-Browser/webpack.config.cjs
+++ src/library-modules/Furaffinity-Features-Browser/webpack.config.cjs
@@ -26,7 +26,7 @@ module.exports = merge(common, {
 // @require     https://update.greasyfork.org/scripts/457759/0000000/FA-Webcomic-Auto-Loader.js
 // @require     https://update.greasyfork.org/scripts/462632/0000000/FA-Infini-Gallery.js
 // @require     https://update.greasyfork.org/scripts/527752/0000000/FA-Instant-Nuker.js
-// @require     https://update.greasyfork.org/scripts/000000/0000000/FA-Watches-Favorites-Viewer.js
+// @require     https://update.greasyfork.org/scripts/463464/0000000/FA-Watches-Favorites-Viewer.js
 // @grant       GM_info
 // @version     1.2.0
 // @author      Midori Dragon
